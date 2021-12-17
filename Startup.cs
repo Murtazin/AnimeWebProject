b791d45b97a61b30de6746827c09bd959f7fe7e4
Murtazin <renatmurtazin@yahoo.com>
2021-12-17 23:16:52 +0300

    added a lot of functionals.

--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using AnimeWebProject.Interfaces;
+using AnimeWebProject.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AnimeWebProject
 {
@@ -25,15 +31,29 @@ namespace AnimeWebProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddCookie(options =>
+            services.AddEntityFrameworkNpgsql()
+                .AddDbContext<ApplicationContext>(option => option.UseNpgsql(
+                Configuration.GetConnectionString("DefaultConnection")));
+            var tokenKey = Configuration.GetValue<string>("TokenKey");
+            var key = Encoding.ASCII.GetBytes(tokenKey);
+            services.AddAuthentication(x =>
+                {
+                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(x =>
+                {
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters
                     {
-                        options.LoginPath = new Microsoft.AspNetCore.Http.PathString("/RegistrationLogin/Login");
-                        options.LogoutPath = new Microsoft.AspNetCore.Http.PathString("/RegLog/Logout");
-                        options.AccessDeniedPath = new Microsoft.AspNetCore.Http.PathString("/Home/Index");
-                        options.ExpireTimeSpan = System.TimeSpan.FromDays(2);
-                    }
-                );
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+            services.AddSingleton<IJWTAuthManager>(new JWTAuthManager(tokenKey));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
